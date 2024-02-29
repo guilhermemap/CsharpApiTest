5cc72a9fda5c0c22fcf046bb5550fdfc548f3df6
guilherme <guilhermemb11@gmail.com>
2024-02-29 15:55:50 -0300

    dbsets do AppDbContext não precisam ser nullable

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -5,6 +5,6 @@ namespace testapi.Context;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-  public DbSet<Categoria>? Categorias { get; set; }
-  public DbSet<Produto>? Produtos { get; set; }
+  public DbSet<Categoria> Categorias { get; set; }
+  public DbSet<Produto> Produtos { get; set; }
 }
