ba10473d1b7cb68a080803ecc8eb1fa1dc76fba9
guilherme <guilhermemb11@gmail.com>
2024-02-29 16:19:00 -0300

    json ignores Categoria from Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace testapi.Models;
 
@@ -21,5 +22,6 @@ public class Produto
   public float Estoque { get; set; }
   public DateTime DataCadastro { get; set; }
   public int CategoriaId { get; set; }
+  [JsonIgnore]
   public Categoria? Categoria { get; set; }
 }
